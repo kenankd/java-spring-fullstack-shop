7be3d1fa4e42d81e376e7ad6816f525f64b890e7
kenankd <kdizdarevi1@etf.unsa.ba>
2023-09-16 18:28:29 +0200

    start, template added

---
+++ src/main/java/com/greenleaf/shop/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.greenleaf.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String showHome(){
+        return "forward:/index.html";
+    }
+}
