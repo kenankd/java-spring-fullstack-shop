799614f920421228f07d67db19d99a1b14f9981e
kenankd <kdizdarevi1@etf.unsa.ba>
2023-09-17 11:48:25 +0200

    added userrepo and userservice

--- src/main/java/com/greenleaf/shop/model/User.java
+++ src/main/java/com/greenleaf/shop/model/User.java
@@ -1,4 +1,63 @@
 package com.greenleaf.shop.model;
 
-public class User{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.annotation.processing.Generated;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
