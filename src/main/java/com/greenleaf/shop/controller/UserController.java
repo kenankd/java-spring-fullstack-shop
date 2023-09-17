4a657f4e1b77859cfd456d1b65bb57d75fa73cde
kenankd <kdizdarevi1@etf.unsa.ba>
2023-09-17 14:11:00 +0200

    user registration to database

--- src/main/java/com/greenleaf/shop/controller/UserController.java
+++ src/main/java/com/greenleaf/shop/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.greenleaf.shop.controller;
 
 import com.greenleaf.shop.model.User;
+import com.greenleaf.shop.service.UserService;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,13 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @Slf4j
 public class UserController {
+    @Autowired
+    private UserService userService;
     @PostMapping("/registerUser")
     public String registerUser(@Valid @ModelAttribute("user") User user, Errors errors){
         if(errors.hasErrors()){
             log.info("Error in validation: " + errors.getAllErrors().toString());
             return "register.html";
         }
-
-        return "redirect:/register";
+        userService.registerUser(user);
+        return "redirect:/home";
     }
 }
--- src/main/java/com/greenleaf/shop/service/UserService.java
+++ src/main/java/com/greenleaf/shop/service/UserService.java
@@ -1,7 +1,19 @@
 package com.greenleaf.shop.service;
 
+import com.greenleaf.shop.model.User;
+import com.greenleaf.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    /**
+     * maybe later return something if went wrong
+     * @param user
+     */
+    public User registerUser(User user){
+        return userRepository.save(user);
+    }
 }
