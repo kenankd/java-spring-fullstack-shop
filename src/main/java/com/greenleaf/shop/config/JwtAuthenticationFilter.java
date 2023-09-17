3feeec4bf04b81c7ad89d7f2c9c4393e77c6e50a
kenankd <kdizdarevi1@etf.unsa.ba>
2023-09-17 19:30:02 +0200

    added jwt service and filter

--- src/main/java/com/greenleaf/shop/config/JwtAuthenticationFilter.java
+++ src/main/java/com/greenleaf/shop/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.greenleaf.shop.config;
 
+import com.greenleaf.shop.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
