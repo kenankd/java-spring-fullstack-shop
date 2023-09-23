04129823d12334e13a7f1a67963ba52ea4baa6c9
kenankd <kdizdarevi1@etf.unsa.ba>
2023-09-23 15:21:47 +0200

    finished contact page

--- src/main/java/com/greenleaf/shop/controller/ContactController.java
+++ src/main/java/com/greenleaf/shop/controller/ContactController.java
@@ -1,6 +1,7 @@
 package com.greenleaf.shop.controller;
 
 import com.greenleaf.shop.model.Contact;
+import com.greenleaf.shop.repository.ContactRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,11 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
+
+    /**
+     * handle database error
+     * @param contact
+     * @param errors
+     * @return
+     */
+
     @PostMapping("/contact/send")
     public String sendMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
         if(errors.hasErrors()){
             return "contact.html";
         }
-        return "redirect:/greenleaf/contact";
+        contactRepository.save(contact);
+        return "redirect:/greenleaf/contact?success=true";
     }
 }
