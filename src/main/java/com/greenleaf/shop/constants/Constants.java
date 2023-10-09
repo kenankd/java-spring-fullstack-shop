00c54ae4b69c77a2cbbcf6a12760a75779f7732b
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-09 22:26:03 +0200

    constants package

---
+++ src/main/java/com/greenleaf/shop/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.greenleaf.shop.constants;
+
+public class Constants {
+    public static String ANY = "any";
+}
--- src/main/java/com/greenleaf/shop/service/ProductService.java
+++ src/main/java/com/greenleaf/shop/service/ProductService.java
@@ -13,17 +13,16 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
-    public Page<Product> findProducts(Integer pageNum, String category, String gender){
-        int pageSize=6;
-        //change this later
-        if(gender!=null && gender.equals("null")) gender=null;
-        if(gender!=null && gender.equals("null")) gender=null;
-        if(category!=null && category.equals("null")) category=null;
-        Pageable pageable=PageRequest.of(pageNum-1,pageSize);
-        if(category==null && gender==null) return productRepository.findAll(pageable);
-        if(category == null) return productRepository.findByGender(gender,pageable);
-        if(gender == null) return productRepository.findByCategory(category,pageable);
-        return productRepository.findByCategoryAndGender(category,gender,pageable);
 
+    public Page<Product> findProducts(Integer pageNum, String category, String gender) {
+        int pageSize = 6;
+        //change this later
+        if (gender != null && gender.equals("null")) gender = null;
+        if (category != null && category.equals("null")) category = null;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        if (category == null && gender == null) return productRepository.findAll(pageable);
+        if (category == null) return productRepository.findByGender(gender, pageable);
+        if (gender == null) return productRepository.findByCategory(category, pageable);
+        return productRepository.findByCategoryAndGender(category, gender, pageable);
     }
 }
